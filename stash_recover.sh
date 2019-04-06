ded2479d75f0fd3c8691c2d25a907b346b30ee0a
ssangmin-park <ssangmin.park@webtoonscorp.com>
2019-04-06 13:28:53 +0900

    Modify commit message searching logic

--- stash_recover.sh
+++ stash_recover.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-commit_hashes=$(git fsck --no-reflog | awk '/dangling commit/ { print $3 }')
+folder=$1
+max_search_days=$2
+
+if [ -z "$folder" ] || [ -z "$max_search_days" ] ; then
+    echo "Usage: $0 <project_folder> <max_search_days>"
+    exit
+fi
+
+commit_hashes=$(git -C $folder fsck --no-reflog | awk '/dangling commit/ { print $3 }')
 
 echo ">>>>>>>>> Begin <<<<<<<<<<"
 echo ""
@@ -9,25 +17,26 @@ tmp_file=$(mktemp)
 
 for commit_hash in $commit_hashes ; do
     # Writing commit log to temp file
-    git show $commit_hash > $tmp_file || true
+    git -C $folder show $commit_hash > $tmp_file || true
 
     # Filtering with 'WIP' and 'No' (it is prefix of stash commit message)
-    commit_message_line=$(cat $tmp_file | grep -n Date: | head -1 | awk -F: '{ print $1 }')
-    commit_message=$(cat $tmp_file | head -$(($commit_message_line + 2)) | tail -1 | xargs)
+    commit_message_line=$(($(cat $tmp_file | grep -n '^$' | head -1 | cut -d ":" -f1) + 1))
+    commit_message=$(cat $tmp_file | head -$commit_message_line | tail -1 | xargs)
+
     commit_message_first_word=$(printf '%s\n' "$commit_message" | awk '{ print $1 }')
 
     if [ "$commit_message_first_word" != "WIP" ] && [ "$commit_message_first_word" != "No" ] ; then
         continue
     fi
 
-    # Filtering within 30 days
+    # Filtering within <max_search_days> days
     commit_rawdate=$(cat $tmp_file | grep Date: | head -1 | cut -d " " -f2-)
     commit_date=$(printf '%s\n' "$commit_rawdate" | awk '{printf "%04d-%02d-%02d\n", $5, (index("JanFebMarAprMayJunJulAugSepOctNovDec",$2)+2)/3 ,$3}')
 
     now_date=$(date +%F)
     diff_days=$(( ($(date -jf %Y-%m-%d $now_date +%s) - $(date -jf %Y-%m-%d $commit_date +%s)) / 86400 ))
 
-    if [ $diff_days -gt 30 ] ; then
+    if [ $diff_days -gt $max_search_days ] ; then
         continue
     fi
 
